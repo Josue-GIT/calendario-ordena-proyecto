bdbf1e81037cbe7fccd6cdffc2a0966df85981d2
unknown <jezc170904@gmail.com>
2025-06-29 19:14:53 -0500

    configurando enrutamiento

--- src/component/navbar/navbar.ts
+++ src/component/navbar/navbar.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbar],
+  imports: [MatToolbar,
+    RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
-
+  constructor(private router: Router) { }
 }
