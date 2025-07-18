07551bb3f4d7eebde629b79118604276a318f2a1
unknown <jezc170904@gmail.com>
2025-07-17 23:06:32 -0500

    calendario actualizado

--- src/component/calendario/calendario.ts
+++ src/component/calendario/calendario.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';  // Importar CommonModule y DatePipe
 
 
 // angular-calendar
@@ -11,8 +12,10 @@ import {
   CalendarModule,
   DateAdapter,
   CalendarView,
+  
 } from 'angular-calendar';
 import { Subject } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
 
 export const colors: any = {
   red: {
@@ -29,22 +32,44 @@ export const colors: any = {
   },
 };
 
+
+
+
 @Component({
   selector: 'app-calendario',
   standalone: true,
   imports: [
     CalendarModule,
+    CommonModule,
+    DatePipe,
+    MatButtonModule,  // Asegurarse de importar DatePipe
   ],
   templateUrl: './calendario.html',
   styleUrls: ['./calendario.scss'],
 })
 export class Calendario {
-  view: string = 'week';
   snapDraggedEvents = true;
 
   dayStartHour = 6;
-  viewDate: Date = new Date();
+  view: CalendarView = CalendarView.Month;  // Vista inicial en mes
+  viewDate: Date = new Date();  // Fecha actual
 
+  setView(view: CalendarView) {
+    this.view = view;
+  }
+
+  previousMonth(): void {
+    this.viewDate = new Date(this.viewDate.setMonth(this.viewDate.getMonth() - 1));
+  }
+
+  nextMonth(): void {
+    this.viewDate = new Date(this.viewDate.setMonth(this.viewDate.getMonth() + 1));
+  }
+
+  today(): void {
+    this.viewDate = new Date();
+  }
+/*
   events: CalendarEvent[] = [
     {
       title: 'Draggable event',
@@ -58,14 +83,15 @@ export class Calendario {
       start: new Date(),
     },
   ];
-
+*/
   refresh: Subject<any> = new Subject();
-
+/*
   eventTimesChanged({ event, newStart, newEnd }: any): void {
     event.start = newStart;
     event.end = newEnd;
     this.refresh.next(null);
   }
+    */
   public segmentIsValid(date: Date) {
     // valid if time is greater than 0800 andd less than 1700
     return date.getHours() >= 8 && date.getHours() <= 17;
@@ -80,4 +106,7 @@ export class Calendario {
     //   });
     // });
   }
+  onDayClick(date: Date): void {
+  alert('Has hecho clic en: ' + date.toLocaleDateString());
+}
 }
